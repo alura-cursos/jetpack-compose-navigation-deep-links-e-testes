9618f1d8294b1430d6dac0709db7ec28475b26ef
Alex Felipe <alexfelipevieira@gmail.com>
2023-01-19 09:59:28 -0300

    4.1

---
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -0,0 +1,26 @@
+package br.com.alura.panucci
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import br.com.alura.panucci.navigation.PanucciNavHost
+import org.junit.Before
+import org.junit.Rule
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            PanucciNavHost(navController = navController)
+        }
+    }
+
+}
