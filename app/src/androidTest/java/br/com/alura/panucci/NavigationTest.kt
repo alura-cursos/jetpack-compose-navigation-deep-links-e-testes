839c633e7293782e8f9e3bedff0d71b22f719e97
Alex Felipe <alexfelipevieira@gmail.com>
2023-01-19 12:51:55 -0300

    4.2

--- app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -2,11 +2,17 @@ package br.com.alura.panucci
 
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import br.com.alura.panucci.navigation.PanucciNavHost
+import br.com.alura.panucci.navigation.highlightsListRoute
+import br.com.alura.panucci.navigation.menuRoute
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class NavigationTest {
 
@@ -21,6 +27,21 @@ class NavigationTest {
             navController.navigatorProvider.addNavigator(ComposeNavigator())
             PanucciNavHost(navController = navController)
         }
+    }
+
+    @Test
+    fun shouldDisplayTheHighlightsListScreenInStartDestination() {
+        val currentRoute = navController.currentBackStackEntry?.destination?.route
+        assertEquals(currentRoute, highlightsListRoute)
+    }
+
+    @Test
+    fun shouldDisplayMenuScreen() {
+        composeTestRule.onNodeWithText("Menu")
+            .performClick()
+
+        val currentRoute = navController.currentBackStackEntry?.destination?.route
+        assertEquals(currentRoute, menuRoute)
     }
 
 }
